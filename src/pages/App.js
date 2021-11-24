0973bda516866e63a01f2892ecd3dda9708ce740
D3NCyan <edwarddoltons@gmail.com>
2021-11-24 10:27:34 +0800

    add routes and LandingPage component

---
+++ src/pages/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom'
+
+// import my component
+import { Header } from '../components'
+import LandingPage from './LandingPage'
+
+// import pages
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,12 @@
+import { logo } from "../../assets"
+
+const LandingPage = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+            Testomg
+        </div>
+    )
+}
+
+export default LandingPage
