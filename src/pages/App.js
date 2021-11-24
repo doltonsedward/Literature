6646f62dd82656e20b9db778715979f32c6de052
D3NCyan <edwarddoltons@gmail.com>
2021-11-24 12:12:48 +0800

    fix code from switch to routes

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // import my component
 import { Header } from '../components'
