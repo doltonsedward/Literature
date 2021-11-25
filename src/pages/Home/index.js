f4ecc8f1f3fe171e8a628e923cf7134883a50ce4
D3NCyan <edwarddoltons@gmail.com>
2021-11-25 08:23:24 +0800

    done Home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,57 @@
 import './_Home.scss'
 import { iconSearch, logo } from "../../assets"
 import { Input, Gap } from '../../components'
+import { useEffect, useState } from 'react'
+import { useHistory } from 'react-router'
+
+// import API
+import { API } from '../../config'
+
+// MUI component
+import { Button } from '@mui/material'
 
 const Home = () => {
+    const history = useHistory()
+    const [dataLiterature, setDataLiterature] = useState([])
+    const [inputData, setInputData] = useState({
+        name: ""
+    })
+
+    const getLiterature = async () => {
+        try {
+            const response = await API.get('/literatures')
+
+            setDataLiterature(response.data.literatures)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleChange = (e) => {
+        setInputData({
+            ...inputData,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    useEffect(()=> {
+        getLiterature()
+    }, [])
+    
     return (
         <div className="homepage">
             <img src={logo} width={489} alt="for searching your book" />
             <Gap height={40} />
             <div className="wrapper-search-list">
-                <Input style={{ width: 600, height: 50 }} placeholder="Search for literature" />
-                <img src={iconSearch} alt="button for searching list book" />
+                <div className="wrapper-input-autocomplete">
+                    <Input style={{ width: 600, height: 50 }} placeholder="Search for literature" list="literature" onChange={handleChange} />
+                    <datalist id="literature" style={{ height: 200, overflowX: 'hidden' }}>
+                        {dataLiterature.map((item, i) => {
+                            return <option key={i} value={item.title} />
+                        })}
+                    </datalist>
+                </div>
+                <Button onClick={()=> history.push('/search-result')}><img src={iconSearch} alt="button for searching list book" /></Button>
             </div>
         </div>
     )
