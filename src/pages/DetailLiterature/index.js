8db8fec77eef56c206e0c550196d95c623152cf3
D3NCyan <edwarddoltons@gmail.com>
2021-11-27 23:17:12 +0800

    put the header on each page instead of on the router

--- src/pages/DetailLiterature/index.js
+++ src/pages/DetailLiterature/index.js
@@ -1,6 +1,7 @@
 import './DetailLiterature.scss'
 import { API } from '../../config/API'
 import { downloadPDF, pushNotif } from '../../utils'
+import { Header } from '../../components'
 
 // MUI component
 import { Button, Typography } from '@mui/material'
@@ -28,48 +29,69 @@ const DetailLiterature = () => {
         }
     }
 
-    // const handleCollection = async () => {
-    //     try {
-    //         await API.post()
-    //     } catch (error) {
+    const handleCollection = async () => {
+        try {
+            await API.post('/collection/' + literature_id)
             
-    //     }
-    // }
+            const status = 'Success'
+            pushNotif({
+                title: status,
+                message: `${literature.title} add to collection`
+            }, status)
+        } catch (error) {
+            pushNotif({
+                title: 'Error',
+                message: 'Unknow error'
+            })
+        }
+    }
+
+    const download = () => {
+        downloadPDF(literature.attache, String(literature.title))
+        const status = 'Success'
+        pushNotif({
+            title: status,
+            message: 'Download literature finished',
+        }, status)
+    }
 
     useEffect(() => {
         getLiterature()
     }, [])
     
     return (
-        <div className="detail-literature literature-default-padding">
-            <div className="section-one">
-                {literature?.attache && <embed src={literature?.attache} />}
-                <ul>
-                    <li>
-                        <Typography variant="h1" component="h1" className="title">{literature?.title}</Typography>
-                        <Typography variant="subtitle2" component="p">{literature?.author}</Typography>
-                    </li>
-                    <li>
-                        <Typography variant="subtitle1" component="h2" className="body">Publication Date</Typography>
-                        <Typography variant="subtitle2" component="p">{literature?.publication_date}</Typography>
-                    </li>
-                    <li>
-                        <Typography variant="subtitle1" component="h2" className="body">Pages</Typography>
-                        <Typography variant="subtitle2" component="p">{literature?.pages}</Typography>
-                    </li>
-                    <li>
-                        <Typography variant="subtitle1" component="h2" className="body isbn">ISBN</Typography>
-                        <Typography variant="subtitle2" component="p">{literature?.pages}</Typography>
-                    </li>
-                    <li>
-                        <Button variant="contained" onClick={()=> downloadPDF(literature.attache, String(literature.title))}>download <img src={iconDownload} alt="download your literature here" /></Button>
-                    </li>
-                </ul>
+        <>
+            <Header />
+            <div className="detail-literature literature-default-padding">
+                <div className="section-one">
+                    {literature?.attache && <embed src={literature?.attache} />}
+                    <ul>
+                        <li>
+                            <Typography variant="h1" component="h1" className="title">{literature?.title}</Typography>
+                            <Typography variant="subtitle2" component="p">{literature?.author}</Typography>
+                        </li>
+                        <li>
+                            <Typography variant="subtitle1" component="h2" className="body">Publication Date</Typography>
+                            <Typography variant="subtitle2" component="p">{literature?.publication_date}</Typography>
+                        </li>
+                        <li>
+                            <Typography variant="subtitle1" component="h2" className="body">Pages</Typography>
+                            <Typography variant="subtitle2" component="p">{literature?.pages}</Typography>
+                        </li>
+                        <li>
+                            <Typography variant="subtitle1" component="h2" className="body isbn">ISBN</Typography>
+                            <Typography variant="subtitle2" component="p">{literature?.pages}</Typography>
+                        </li>
+                        <li>
+                            <Button variant="contained" onClick={download}>download <img src={iconDownload} alt="download your literature here" /></Button>
+                        </li>
+                    </ul>
+                </div>
+                <div className="section-two">
+                    <Button variant="contained" onClick={handleCollection}>add to collection <img src={iconCollection} alt="add to your collection" /></Button>
+                </div>
             </div>
-            <div className="section-two">
-                <Button variant="contained">add to collection <img src={iconCollection} alt="add to your collection" /></Button>
-            </div>
-        </div>
+        </>
     )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import './_Home.scss'
 import React from 'react';
 import { iconSearch, logo } from "../../assets"
-import { Input, Gap } from '../../components'
+import { Input, Gap, Header, SearchResult } from '../../components'
 
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
@@ -17,7 +17,9 @@ import { Button } from '@mui/material'
 const Home = () => {
     const currentState = useSelector(state => state)
     const history = useHistory()
+
     const [dataLiterature, setDataLiterature] = useState([])
+    const [isSearchActive, setIsSearchActive] = useState(false)
     const [inputData, setInputData] = useState({
         title: ""
     })
@@ -57,26 +59,33 @@ const Home = () => {
     }, [])
 
     const handleSubmit = () => {
-        localStorage.setItem("search", JSON.stringify(inputData))
-        history.push('/search-result')
+        setIsSearchActive(true)
     }
     
     return (
-        <div className="homepage literature-default-padding">
-            <img src={logo} width={489} alt="for searching your book" />
-            <Gap height={40} />
-            <div className="wrapper-search-list">
-                <div className="wrapper-input-autocomplete">
-                    <Input style={{ width: 600, height: 50 }} name="title" value={inputData.title} list="literature" onChange={handleChange} placeholder="Search for literature" />
-                    <datalist id="literature" style={{ height: 200, overflowX: 'hidden' }}>
-                        {dataLiterature.map((item, i) => {
-                            return <option key={i} value={item.title} />
-                        })}
-                    </datalist>
+        <>
+            <Header />
+            {
+                isSearchActive ? 
+                <SearchResult searchKey={inputData} />
+                :
+                <div className="homepage literature-default-padding">
+                    <img src={logo} width={489} alt="for searching your book" />
+                    <Gap height={40} />
+                    <div className="wrapper-search-list">
+                        <div className="wrapper-input-autocomplete">
+                            <Input style={{ width: 600, height: 50 }} name="title" value={inputData.title} list="literature" onChange={handleChange} placeholder="Search for literature" />
+                            <datalist id="literature" style={{ height: 200, overflowX: 'hidden' }}>
+                                {dataLiterature.map((item, i) => {
+                                    return <option key={i} value={item.title} />
+                                })}
+                            </datalist>
+                        </div>
+                        <Button onClick={handleSubmit}><img src={iconSearch} alt="button for searching list book" /></Button>
+                    </div>
                 </div>
-                <Button onClick={handleSubmit}><img src={iconSearch} alt="button for searching list book" /></Button>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 
