167517b7a2e6a0a89dc5c7d302a298392d0ea7de
D3NCyan <edwarddoltons@gmail.com>
2021-11-30 09:33:41 +0800

    separate loading components

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -3,4 +3,5 @@ import GoogleLoginBtn from './GoogleLoginBtn'
 import Input from './Input'
 import MuiAlert from './MuiAlert'
 
+export * from './Loading'
 export { Gap, GoogleLoginBtn, Input, MuiAlert }
