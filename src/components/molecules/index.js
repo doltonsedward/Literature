e74607dac5f2491f93a63f24ed774b5b33a6b698
D3NCyan <edwarddoltons@gmail.com>
2021-12-03 17:39:36 +0800

    done clean up code molecules & organism 3 dec, 5:39

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -4,10 +4,9 @@ import ListOwnerLiterature from './ListOwnerLiterature'
 import Login from './Login'
 import Register from './Register'
 import ProfileFAQ from './ProfileFAQ'
-import SearchResult from './SearchResult'
 import TabPanel from './TabPanel'
 
-export { Header, BlankImage, ListOwnerLiterature, Login, Register, SearchResult, ProfileFAQ, TabPanel }
+export { Header, BlankImage, ListOwnerLiterature, Login, Register, ProfileFAQ, TabPanel }
 export * from './Box'
 export * from './Dialog'
 export * from './List'
--- src/components/organism/index.js
+++ src/components/organism/index.js
@@ -1,3 +1,5 @@
+import ContentSearchList from "./ContentSearchList"
 import TabsProfile from "./TabsProfile"
 
-export { TabsProfile }
+export * from './SectionDetailLiterature'
+export { ContentSearchList, TabsProfile }
