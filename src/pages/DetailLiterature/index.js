4a5fa8607edcf0620fc233f09c06ba9d6db5b741
D3NCyan <edwarddoltons@gmail.com>
2021-12-03 17:33:17 +0800

    separate component DetailLiterature to templates

--- src/pages/DetailLiterature/index.js
+++ src/pages/DetailLiterature/index.js
@@ -1,25 +1,9 @@
-import './DetailLiterature.scss'
 import { API } from '../../config/API'
 import { downloadPDF } from '../../utils'
-import { iconCollection } from '../../assets'
-import { DialogDetailLiterature, Header, ListDetailLiterature } from '../../components'
+import { ContainerDetailLiterature, Header } from '../../components'
 import { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router'
 import { toast } from 'react-toastify'
-
-// PDF render
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
-
-// MUI component
-import BookmarkRemoveIcon from '@mui/icons-material/BookmarkRemove';
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { useTheme } from '@mui/material/styles';
-import { 
-    useMediaQuery,
-    Typography,
-    Button
-} from '@mui/material'
 import { useSelector } from 'react-redux'
 
 const DetailLiterature = () => {
@@ -27,12 +11,7 @@ const DetailLiterature = () => {
     const currentState = useSelector(state => state)
     const { literature_id } = useParams()
 
-    // MUI state and logic
     const [openDialog, setOpenDialog] = useState(false)
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('md'))
-    // close
-
     const [inputMakeSure, setInputMakeSure] = useState('')
     const [numPages, setNumPages] = useState(null)
     const [pageNumber, setPageNumber] = useState(1)
@@ -88,11 +67,13 @@ const DetailLiterature = () => {
             if (response.status === 'success') return toast.success(`${literature.title} delete finished`)
             history.push('/profile')
         } catch (error) {
-            toast.error("Unknow error")
+            const message = error?.response?.data.message || 'Unknow error'
+            toast.error(message)
         }
     }
 
     const handler = {
+        handleCollection,
         handleCheckInput: (e) => setInputMakeSure(e.target.value),
         changePage: (offset) => setPageNumber(prevPageNumber => prevPageNumber + offset), // handle when page pdf change
         previousPage: () => handler.changePage(-1), // handle to prev page pdf
@@ -118,106 +99,20 @@ const DetailLiterature = () => {
         getCollection()
     }, [])
 
-    const buttonAuthor = {
-        color: 'lightblue',
-        border: '1px solid lightblue',
-        '&:hover': {
-            border: '1px solid lightblue',
-            opacity: '.7'
-        }
-    }
-
-    const buttonDelete = {
-        color: 'var(--warning)',
-        border: '1px solid var(--warning)',
-        '&:hover': {
-            border: '1px solid var(--warning)',
-            opacity: '.7'
-        }
-    }
-
     const author = currentState.user.fullName
     
+    const dataToChild = { currentState, author }
+    const dataDialog = { openDialog, setOpenDialog }
+    const stateToChild = { literature, inputMakeSure, numPages, pageNumber, collection }
+
     return (
         <>
             <Header />
-            <div className="detail-literature literature-default-padding">
-                <div className="section-one">
-                    <div className="wrapper-pdf">
-                        <div className="button-pages" onClick={(e)=> e.stopPropagation()}>
-                            <div className="inner-button-pages">
-                                <Button 
-                                    variant="contained"
-                                    className="prev-button btn"
-                                    disabled={pageNumber <= 1} 
-                                    onClick={handler.previousPage}
-                                >
-                                    <KeyboardArrowLeftIcon />
-                                </Button>
-                                <Typography variant="subtitle1" component="span" className="detail">{pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}</Typography>
-                                <Button
-                                    variant="contained"
-                                    className="next-button btn"
-                                    disabled={pageNumber >= numPages}
-                                    onClick={handler.nextPage}
-                                >
-                                    <KeyboardArrowRightIcon />
-                                </Button>
-                            </div>
-                        </div>
-                        {literature?.attache && (
-                            <Document
-                                file={literature.attache}
-                                onLoadSuccess={handler.onDocumentLoadSuccess}
-                                options={{ workerSrc: "/pdf.worker.js" }}
-                                className="pdf-reader"
-                            >
-                                <Page 
-                                    pageNumber={pageNumber} 
-                                    renderTextLayer={false}
-                                />
-                            </Document>
-                        )}
-                    </div>
-                    <ListDetailLiterature 
-                        data={literature}
-                        onDownload={handler.download}
-                        numPages={numPages}
-                    />
-                </div>
-                <div className="section-two">
-                    {
-                        literature.ownerLiterature?.fullName !== currentState.user.fullName ?
-                        <Button variant="contained" onClick={handleCollection}>
-                            {
-                                collection ?
-                                <p>remove from collection <BookmarkRemoveIcon className="icon" /></p>
-                                : 
-                                <p>add to collection <img src={iconCollection} className="icon" alt="add to your collection" /></p>
-                            } 
-                            
-                        </Button>
-                        : 
-                        literature.status === 'Cancel' ?
-                        <Button variant="outlined" sx={buttonDelete} onClick={()=> setOpenDialog(true)}>
-                            <p>delete literature</p>
-                        </Button>
-                        :
-                        <Button variant="outlined" sx={buttonAuthor}>
-                            <p>you are the author</p>
-                        </Button>
-                    }
-                </div>
-            </div>
-            <DialogDetailLiterature 
-                author={author}
-                fullScreen={fullScreen}
-                open={openDialog}
-                setOpen={setOpenDialog}
-                onAgree={handler.onAgree}
-                onDisagree={handler.onDisagree}
-                onCheckInput={handler.handleCheckInput}
-                inputMakeSure={inputMakeSure}
+            <ContainerDetailLiterature 
+                otherData={dataToChild}
+                dataDialog={dataDialog}
+                handler={handler} 
+                getter={stateToChild}
             />
         </>
     )
