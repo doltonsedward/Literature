0f92444ac61e6b8244c0c179f5263eca5c2700f0
D3NCyan <edwarddoltons@gmail.com>
2021-12-03 17:36:41 +0800

    separate SearchResult to templates

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,9 @@
 import './_Home.scss'
 import React from 'react';
 import { iconSearch, logo } from "../../assets"
-import { Input, Gap, Header, SearchResult } from '../../components'
+import { Input, Gap, Header, ContainerSearchResult } from '../../components'
 
 import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 
 // import API
@@ -14,30 +13,43 @@ import { API, checkUser, setAuthToken } from '../../config'
 import { Button } from '@mui/material'
 
 const Home = () => {
-    const currentState = useSelector(state => state)
-
     const [dataLiterature, setDataLiterature] = useState([])
     const [isSearchActive, setIsSearchActive] = useState(false)
     const [inputData, setInputData] = useState({
-        title: ""
+        title: '',
+        public_year: ""
     })
 
-    const getLiterature = async () => {
+    const getLiterature = async (type) => {
         try {
-            const response = await API.get('/literatures')
-
+            let response 
+            if (type === 'search') {
+                response = await API.get(`/search-literatures?${inputData.title ? `title=${inputData.title}&` : ''}${inputData.public_year ? `public_year=${inputData.public_year}` : ''}`)
+            } else {
+                response = await API.get('/literatures')
+            }
+            
             setDataLiterature(response.data.literatures)
         } catch (error) {
-            const message = error?.response?.data.message || 'Unknow error'
+            const message = error.response?.data?.message || 'Unknow error'
             toast.error(message)
         }
     }
 
-    const handleChange = (e) => {
-        setInputData({
-            ...inputData,
-            [e.target.name]: e.target.value
-        })
+    const handler = {
+        handleChange: (e) => {
+            setInputData({
+                ...inputData,
+                [e.target.name]: e.target.value
+            })
+        },
+        handleSubmit: () => {
+            setIsSearchActive(true)
+            getLiterature('search')
+        },
+        handleSearch: () => {
+            getLiterature('search')
+        }
     }
 
     // for fetching setauth again
@@ -45,37 +57,40 @@ const Home = () => {
         if (localStorage.token) {
             setAuthToken(localStorage.token)
         }
-    }, [currentState])
-    
+    }, [])
+
     useEffect(()=> {
         getLiterature()
         checkUser()
     }, [])
 
-    const handleSubmit = () => {
-        setIsSearchActive(true)
-    }
+    const fetchingHandler = { getLiterature }
+    const stateToChild = { dataLiterature, inputData }
     
     return (
         <>
             <Header />
             {
                 isSearchActive ? 
-                <SearchResult searchKey={inputData} />
+                <ContainerSearchResult 
+                    fetching={fetchingHandler}
+                    handler={handler}
+                    getter={stateToChild}
+                />
                 :
                 <div className="homepage literature-default-padding">
                     <img src={logo} width={489} alt="for searching your book" />
                     <Gap height={40} />
                     <div className="wrapper-search-list">
                         <div className="wrapper-input-autocomplete">
-                            <Input style={{ width: 600, height: 50 }} name="title" value={inputData.title} list="literature" onChange={handleChange} placeholder="Search for literature" />
+                            <Input style={{ width: 600, height: 50 }} name="title" value={inputData.title} list="literature" onChange={handler.handleChange} placeholder="Search for literature" />
                             <datalist id="literature" style={{ height: 200, overflowX: 'hidden' }}>
                                 {dataLiterature.map((item, i) => {
                                     return <option key={i} value={item.title} />
                                 })}
                             </datalist>
                         </div>
-                        <Button onClick={handleSubmit}><img src={iconSearch} alt="button for searching list book" /></Button>
+                        <Button onClick={handler.handleSubmit}><img src={iconSearch} alt="button for searching list book" /></Button>
                     </div>
                 </div>
             }
